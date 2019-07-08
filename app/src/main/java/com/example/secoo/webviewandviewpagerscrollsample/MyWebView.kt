d818166ccd9fcb3cf250202a9c4bc9a85b9b8fe9
yuany <yuany@2345.com>
2019-07-08 20:38:24 +0800

    1. 增加递归最大深度判断 2. 不用每次都获取可以滑动的父控件 3. 修改横向滑动的判断

--- app/src/main/java/com/example/secoo/webviewandviewpagerscrollsample/MyWebView.kt
+++ app/src/main/java/com/example/secoo/webviewandviewpagerscrollsample/MyWebView.kt
@@ -8,6 +8,7 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewParent
 import android.webkit.WebView
 import android.widget.AbsListView
@@ -19,6 +20,8 @@ class MyWebView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : WebView(context, attrs, defStyleAttr) {
 
+    private var scrollParent: ViewGroup? = null
+
     private fun dumpMessage(message: String) {
         Log.i(LOGTAG, "dumpMessage message=$message")
     }
@@ -26,8 +29,10 @@ class MyWebView @JvmOverloads constructor(
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_DOWN) {
-            val viewParent = findViewParentIfNeeds(this)
-            viewParent?.requestDisallowInterceptTouchEvent(true)
+            if (scrollParent == null){
+                scrollParent = findViewParentIfNeeds(this, 10)
+            }
+            scrollParent?.requestDisallowInterceptTouchEvent(true)
         }
         return super.onTouchEvent(event)
     }
@@ -36,31 +41,34 @@ class MyWebView @JvmOverloads constructor(
         dumpMessage("onOverScrolled scrollX=" + scrollX + ";scrollY=" + scrollY
                 + ";clampedX=" + clampedX + ";clampedY=" + clampedY)
         if (clampedX) {
-            val viewParent = findViewParentIfNeeds(this)
-            viewParent?.requestDisallowInterceptTouchEvent(false)
+            if (scrollParent != null){
+                scrollParent = findViewParentIfNeeds(this, 10)
+            }
+            scrollParent?.requestDisallowInterceptTouchEvent(false)
         }
         super.onOverScrolled(scrollX, scrollY, clampedX, clampedY)
     }
 
-    internal fun findViewParentIfNeeds(tag: View): ViewParent? {
-        val parent = tag.parent
-        if (parent == null) {
-            return parent
+    internal fun findViewParentIfNeeds(tag: View, depth: Int): ViewGroup? {
+        if (depth < 0) {
+            return null
         }
-        return if (parent is ViewPager ||
-                parent is AbsListView ||
-                parent is ScrollView ||
-                parent is HorizontalScrollView ||
-                parent is GridView) {
-            parent
-        } else {
-            if (parent is View) {
-                findViewParentIfNeeds(parent as View)
-            } else {
-                parent
+        val parent = tag.parent?: return null
+        if (parent is ViewGroup){
+            if (canScrollHorizontal(parent)){
+                return parent
+            }else {
+                //增加最大地柜深度判断，防止出现ANR或者异常
+                findViewParentIfNeeds(parent, depth-1)
             }
         }
+        return null
     }
+
+    /**
+     * [view] 是否可以横向滑动
+     */
+    private fun canScrollHorizontal(view: View) = view.canScrollHorizontally(100) || view.canScrollHorizontally(-100)
 
     companion object {
         private val LOGTAG = "MyWebView"
